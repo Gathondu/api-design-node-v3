1803fd4c6a71dc5cdeea8d23a11540f9e7583ffd
Marc Grabanski <m@marcgrabanski.com>
2019-01-14 19:50:57 -0600

    Create routes for the item resource

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
-import { Router } from 'express'
+import {Router} from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+// api/item/
+router.route('/')
+  .get(controllers.getMany)
+  .post(controllers.createOne)
+
+// api/item/:id
+
+router.route('/:id')
+  .put(controllers.updateOne)
+  .get(controllers.getOne)
+  .delete(controllers.removeOne)
 
 export default router
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+import {json, urlencoded} from 'body-parser'
+import cors from 'cors'
 import express from 'express'
-import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
-import cors from 'cors'
-import { connect } from './utils/db'
+import itemRoues from './resources/item/item.router'
+import listRoutes from './resources/list/list.router'
+import userRoutes from './resources/user/user.router'
+import {connect} from './utils/db'
 
 export const app = express()
 
@@ -13,6 +16,10 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+app.use('/api/item', itemRoues)
+app.use('/api/list', listRoutes)
+app.use('/api/user', userRoutes)
 
 export const start = async () => {
   try {
